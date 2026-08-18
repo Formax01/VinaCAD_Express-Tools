bdbc35fb4eab3e05d25d2e709ae92a3c1aa2dff4
hungnguyen <hungwork2109@gmail.com>
2026-08-18 17:42:41 +0700

    commit update Command: trim và fix tường (TW)

--- Tools.VinaCad.App/Commands/Commands.cs
+++ Tools.VinaCad.App/Commands/Commands.cs
@@ -523,5 +523,20 @@ namespace Tools.VinaCAD.App.Commands
                 Logger.Info(nameof(EraseAutoHealWallCommand), ex);
             }
         }
+
+        [CommandMethod("TW")]
+        public void TrimFixWallCommand()
+        {
+            try
+            {
+                TrimFixWallAction action = new TrimFixWallAction();
+                action.Execute();
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message, StringDefinition.TITLE_ERROR);
+                Logger.Info(nameof(TrimFixWallCommand), ex);
+            }
+        }
     }
 }
