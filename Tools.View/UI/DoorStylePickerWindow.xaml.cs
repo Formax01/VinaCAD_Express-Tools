7ec973660f49cbeb89d7c5c5f922143c5d2ac176
hungnguyen <hungwork2109@gmail.com>
2026-09-18 15:40:16 +0700

    commit chỉnh lại,xóa code behind UI

--- Tools.View/UI/DoorStylePickerWindow.xaml.cs
+++ Tools.View/UI/DoorStylePickerWindow.xaml.cs
@@ -1,7 +1,4 @@
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Media;
 using Tools.Model;
 using Tools.ViewModel;
 
@@ -16,28 +13,6 @@ namespace Tools.View.UI
         {
             InitializeComponent();
             DataContext = new DoorStylePickerVM(catalog, initialStyle);
-            PreviewKeyDown += Window_PreviewKeyDown;
-        }
-
-        private void StyleList_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            DependencyObject? element = e.OriginalSource as DependencyObject;
-            while (element != null && element is not ListBoxItem)
-                element = VisualTreeHelper.GetParent(element);
-            if (element is not ListBoxItem item || item.DataContext is not DoorStyleModel style) return;
-            ViewModel.SelectedStyle = style;
-            DialogResult = true;
-        }
-
-        private void Previous_Click(object sender, RoutedEventArgs e) => ViewModel.PreviousPage();
-        private void Next_Click(object sender, RoutedEventArgs e) => ViewModel.NextPage();
-        private void Cancel_Click(object sender, RoutedEventArgs e) => DialogResult = false;
-
-        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key != Key.Escape) return;
-            e.Handled = true;
-            DialogResult = false;
         }
     }
 }
