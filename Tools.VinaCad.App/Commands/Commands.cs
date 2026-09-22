d023e914dd267283fd0ec918058c696c2595e83d
hungnguyen2004-knight <hungwork2109@gmail.com>
2026-09-22 15:19:12 +0700

    commit nhần action execute AD và AW

--- Tools.VinaCad.App/Commands/Commands.cs
+++ Tools.VinaCad.App/Commands/Commands.cs
@@ -617,7 +617,7 @@ namespace Tools.VinaCAD.App.Commands
         {
             try
             {
-                new ArrangeWindowAction().Execute();
+                new OpenDoorAction().Execute();
             }
             catch (System.Exception ex)
             {
@@ -636,7 +636,7 @@ namespace Tools.VinaCAD.App.Commands
               catch (System.Exception ex)
               {
                   MessageBox.Show(ex.Message, StringDefinition.TITLE_ERROR);
-                  Logger.Info(nameof(OpenDoorCommand), ex);
+                  Logger.Info(nameof(ArrangeWindowCommand), ex);
               }
           }
     }
