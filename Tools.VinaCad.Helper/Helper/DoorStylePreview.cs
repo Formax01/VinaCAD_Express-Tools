9f42e7b9c031b0f9d2124ba00c77f1100725d3d5
hungnguyen2004-knight <hungwork2109@gmail.com>
2026-09-24 16:05:18 +0700

    commit updatw nét màu vẽ chuyển preview door về helper

--- Tools.VinaCad.Helper/Helper/DoorStylePreview.cs
+++ Tools.VinaCad.Helper/Helper/DoorStylePreview.cs
@@ -9,8 +9,8 @@ namespace Tools.View.Controls
 {
     public sealed class DoorStylePreview : FrameworkElement
     {
-        private static readonly Brush Background = CreateBrush(31, 38, 48);
-        private static readonly Pen GeometryPen = CreatePen(CreateBrush(51, 255, 51), 1.5);
+        private static readonly Brush Background = CreateBrush(33, 40, 48);
+        private static readonly Pen GeometryPen = CreatePen(CreateBrush(0, 127, 127), 2.0);
 
         public static readonly DependencyProperty DoorStyleProperty = DependencyProperty.Register(
             nameof(DoorStyle),
